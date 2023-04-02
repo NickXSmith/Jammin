2c2804c5bbe45db6d0641f93dbcee67e61f6e645
Nick X Smith <geeknxs@gmail.com>
2023-04-02 15:11:57 -0600

    testing changes to implement the no display playlist feature

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,7 +11,7 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-        searchResults: [],
+      searchResults: [],
       playlistName: "Playlist Name",
       playlistTracks: []
     };
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -4,16 +4,28 @@ import './TrackList.css';
 import { Track } from '../Track/Track';
 
 class TrackList extends React.Component {
-       render() {
+
+    inInPlaylist(track) {
+        let tracksIn = this.props.playlistTracks;
+        if (!tracksIn) {
+            return
+        }
+        return (tracksIn.find(savedTrack =>(savedTrack.id === track.id))) ? true : false;
+    }   
+    render() {
         return (
             <div className="TrackList">
                 {
                     this.props.tracks.map(track => {
-                        return <Track track={track}
+                        if (!this.inInPlaylist(track)) {
+                            return <Track track={track}
                             key={track.id} 
                             onAdd={this.props.onAdd}
                             onRemove={this.props.onRemove}
                             isRemoval={this.props.isRemoval}/>
+                    } else {
+                        return null;
+;                    } 
                     })
                 }
             </div>
